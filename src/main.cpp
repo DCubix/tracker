a430f2e3f3ce43d268a4f4ba32e88fcc3dfc2e24
twister <diego95lopes@live.com>
2020-04-07 13:25:57 -0400

    Vibrato, portamento, ADSR envelope

--- src/main.cpp
+++ src/main.cpp
@@ -70,10 +70,98 @@ private:
 	std::array<float, 24> m_waveTable{};
 };
 
+class ADSR {
+public:
+	enum State {
+		Idle = 0,
+		Attack,
+		Decay,
+		Sustain,
+		Release
+	};
+
+	ADSR()
+		: m_attack(0.0f), m_decay(0.0f), m_sustain(1.0f), m_release(0.0f)
+	{}
+
+	ADSR(float a, float d, float s, float r)
+	{
+		attack(a);
+		decay(d);
+		sustain(s);
+		release(r);
+	}
+
+	float attack() const { return m_attack; }
+	void attack(float v) { m_attack = v; }
+
+	float decay() const { return m_decay; }
+	void decay(float v) { m_decay = v; }
+
+	float release() const { return m_release; }
+	void release(float v) { m_release = v; }
+
+	float sustain() const { return m_sustain; }
+	void sustain(float v) { m_sustain = v; }
+
+	void gate(bool g) {
+		if (g) {
+			m_state = Attack;
+		} else if (m_state != Idle) {
+			m_state = Release;
+		}
+	}
+
+	float sample(float rate) {
+		switch (m_state) {
+			default: break;
+			case State::Attack: {
+				m_out += (1.0f / m_attack) / rate;
+				if (m_out >= 1.0f || m_attack <= 1e-5f) {
+					m_state = State::Decay;
+					m_out = 1.0f;
+				}
+			} break;
+			case State::Decay: {
+				m_out -= (1.0f / m_decay) / rate;
+				if (m_out <= m_sustain || m_decay <= 1e-5f) {
+					m_out = m_sustain;
+					m_state = State::Sustain;
+				}
+			} break;
+			case State::Release: {
+				m_out -= (1.0f / m_release) / rate;
+				if (m_out <= 1e-5f) {
+					m_out = 0.0;
+					m_state = State::Idle;
+				}
+			} break;
+		}
+		return m_out;
+	}
+
+	void reset() {
+		m_state = State::Release;
+		m_out = 0.0f;
+	}
+
+	float value() const { return m_out; }
+
+private:
+	State m_state;
+	float m_attack,
+		m_decay,
+		m_sustain,
+		m_release,
+		m_out;
+};
+
 struct Event {
-	enum { NoteOn = 0, NoteOff } type{ NoteOff };
+	enum Type { NoteOn = 0, NoteOff };
+	Type type{ NoteOff };
 	int note{ 0 };
 	int length{ 1 };
+	bool vibrato{ false };
 };
 
 static const float NOTE[] = {
@@ -91,46 +179,98 @@ static const float NOTE[] = {
 	61.73541f
 };
 
-class Track {
+enum Note {
+	C = 0,
+	Cs,
+	D,
+	Ds,
+	E,
+	F,
+	Fs,
+	G,
+	Gs,
+	A,
+	As,
+	B
+};
+
+class Channel {
 public:
-	Track() {
+	Channel() {
 		m_waveTable.setWaveForm(WaveTable::Sine);
+		m_amplitudeEnv.attack(0.05f);
+		m_amplitudeEnv.decay(0.45f);
+		m_amplitudeEnv.sustain(0.0f);
+		m_amplitudeEnv.release(0.5f);
 	}
 
 	float sample() {
+		if (m_events.empty()) return 0.0f;
+
 		const float delay = (60000.0f / BPM) / 1000.0f;
-		m_time += (1.0f / SR) * 4.0f;
+
+		m_time += (1.0f / SR) * 8.0f;
 		if (m_time >= delay) {
-			if (m_current == nullptr) {
-				m_position %= m_events.size();
-				m_current = &m_events[m_position++];
-				m_timeUnits = m_current->length;
-			} else {
-				if (m_timeUnits-- <= 0) {
-					m_current = nullptr;
+			if (m_timeUnits-- < 0) {
+				if (m_current != nullptr) {
+					float freq = NOTE[m_current->note % 12] * std::pow(2, m_current->note / 12);
+					m_prevFreq = freq;
+				}
+				m_current = nullptr;
+				if (m_amplitudeEnv.value() > 0.0f) {
+					m_amplitudeEnv.gate(false);
 				}
 			}
-			m_time -= delay;
+			m_time = 0.0f;
 		}
 
-		if (m_current != nullptr) {
-			return m_waveTable.sample(NOTE[m_current->note % 12] * std::pow(2, m_current->note / 12));
+		if (m_current == nullptr && m_position < m_events.size()) {
+			m_current = &m_events[m_position++];
+			m_timeUnits = m_current->length;
+			
+			m_amplitudeEnv.gate(true);
+		}
+
+		if (m_current != nullptr && m_current->type == Event::NoteOn) {
+			float freq = NOTE[m_current->note % 12] * std::pow(2, m_current->note / 12);
+
+			if (m_portamento && m_timeUnits == m_current->length) {
+				freq = LERP(m_prevFreq, freq, m_time / delay);
+			}
+
+			if (m_current->vibrato) {
+				freq += std::cos(m_vibratoPhase.get((1.0f / 60.0f) * BPM * 4.0f)) * (NOTE[2] - NOTE[0]);
+			}
+
+			return m_waveTable.sample(freq) * m_amplitudeEnv.sample(SR);
 		}
 		return 0.0f;
 	}
 
-	std::vector<Event>& events() { return m_events; }
+	void pushNote(int note, int length = 1, Event::Type type = Event::NoteOn, bool vibrato = false) {
+		Event ev{};
+		ev.note = note;
+		ev.length = length;
+		ev.type = type;
+		ev.vibrato = vibrato;
+		m_events.push_back(ev);
+	}
 
 private:
 	WaveTable m_waveTable{};
+	ADSR m_amplitudeEnv{};
+	Phase m_vibratoPhase{};
+
+	bool m_portamento{ true };
+
 	std::vector<Event> m_events{};
-	float m_time{ 0.0f };
+	float m_time{ 0.0f }, m_prevFreq{ 0.0f };
 
 	Event* m_current{ nullptr };
 	int m_timeUnits, m_position{ 0 };
 };
 
-static Track p{};
+static Channel p{};
 
 void callback(void* udata, Uint8* stream, int len) {
 	for (int i = 0; i < len; i++) {
@@ -147,21 +287,23 @@ int main(int argc, char** argv) {
 	spec.samples = 1024;
 	spec.callback = callback;
 
-	Event ev{};
-	ev.note = 32;
-	ev.length = 1;
-	p.events().push_back(ev);
-
-	Event ev2{};
-	ev2.note = 34;
-	ev2.length = 1;
-	p.events().push_back(ev2);
+	p.pushNote(Note::C + 24, 2);
+	p.pushNote(Note::D + 24, 2);
+	p.pushNote(Note::E + 24, 2);
+	p.pushNote(Note::F + 24, 2);
+	p.pushNote(Note::G + 24, 2);
+	p.pushNote(Note::A + 24, 2);
+	p.pushNote(Note::B + 24, 2);
+	p.pushNote(Note::C + 36, 2);
+	// for (int i = 24; i < 34; i++) {
+	// 	p.pushNote(i, 1);
+	// }
 
 	if (SDL_OpenAudio(&spec, NULL) < 0) {
 		return -1;
 	}
 	SDL_PauseAudio(0);
-	SDL_Delay(2000);
+	SDL_Delay(3000);
 	SDL_CloseAudio();
 	return 0;
 }
